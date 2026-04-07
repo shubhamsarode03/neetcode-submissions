5dbf271f1572b0fdb6867915a15bc9d0cec8ed3e
shubhamsarode03 <118796717+shubhamsarode03@users.noreply.github.com>
2026-04-07 23:10:53 +0530

    Add: reverse-string - submission-5

---
+++ Data Structures & Algorithms/reverse-string/submission-5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for (int i = 0; i < n - 1; i++) {
+            // Take the last element and move it to the 'i-th' position
+            char lastElement = s[n - 1];
+            for (int j = n - 1; j > i; j--) {
+                s[j] = s[j - 1];
+            }
+            s[i] = lastElement;
+        }
+    }
+}
